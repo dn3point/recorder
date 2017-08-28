138a0b9808d984a45f57169c928e021ce1b84519
uenhoeyz <uenhoeyz@gmail.com>
2017-08-28 20:46:40 +0800

    Revert api key

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,11 @@
 // Initialize Firebase
 var config = {
-
+    apiKey: "AIzaSyAJxanwQ18NULfUaIAdb0OOKqj0-_-r2wo",
+    authDomain: "recorder-9733b.firebaseapp.com",
+    databaseURL: "https://recorder-9733b.firebaseio.com",
+    projectId: "recorder-9733b",
+    storageBucket: "recorder-9733b.appspot.com",
+    messagingSenderId: "286592197717"
 };
 firebase.initializeApp(config);
 
